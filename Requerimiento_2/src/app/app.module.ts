b744317abcbcd377ade667ae94a6aea8f60ef73c
spinelf@gmail.com <spinelf@gmail.com>
2022-11-29 21:44:38 +0100

    Creación componentes

--- Requerimiento_2/src/app/app.module.ts
+++ Requerimiento_2/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      FormularioComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
---
+++ Requerimiento_2/src/app/formulario/formulario.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../models/Usuario';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+export class FormularioComponent implements OnInit {
+
+ 
+    public usuarios: Array<Usuario>;
+
+    constructor() { 
+       this.usuarios=[
+        new Usuario ('spinelf','12345'),
+        new Usuario ('souldeep','12345')
+      
+      ];
+
+  }
+
+  ngOnInit() {
+  }
+
+}
